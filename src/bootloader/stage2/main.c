8a6a8b95e8f95cf10c6a096a0bd6b7ec94215aac
HarmoniousHippo09 <nvhm09@gmail.com>
2025-05-03 13:05:11 +0700

    Fixed VGA display bug

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,11 +1,16 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "interrupt/interrupt.h"
-
+#include "timer/pit.h"
+#include "disk/disk.h"
 
 void __attribute__((cdecl)) start(u16 bootDrive, void* partitionOffset)
 {
     interrupt_init();
+    PIT_init(2000);
+    disk_init(0, partitionOffset); // Mount our drive and partition
+    // Welp, I guess we can write a FAT32 filesystem driver now, eh?
+
     kprintf("Hello from stage2!\n");
 end:    
     for (;;);
--- src/bootloader/stage2/vga/vga.c
+++ src/bootloader/stage2/vga/vga.c
@@ -24,15 +24,41 @@ void putcolor(u32 x, u32 y, u8 fg, u8 bg)
     vram[2*(y*SW+x)+1] = (bg&0x7) << 4 | (fg&0xF);
 }
 
+void putcolor_internal(u32 x, u32 y, u8 color)
+{
+    vram[2*(y*SW+x)+1] = color;
+}
+
+char getch(u32 x, u32 y)
+{
+    return vram[2*(y*SW+x)];
+}
+
+u8 getcolor(u32 x, u32 y)
+{
+    return vram[2*(y*SW+x)+1];
+}
+
 void scroll(int lines)
 {
-    // TODO
+    u8 col = getcolor(0,0);
     for (int i = lines; i < SH; i++)
     {
-        memmove((void*)(vram+2*(i-lines)*SW), (const void*)(vram+2*i*SW), 2*SW);
+        for (int j=0;j<SW;j++)
+        {
+            putcolor_internal(j, i, getcolor(j, i-lines));
+            putch(j, i, getch(j, i-lines));
+        }
     }
 
-    memset((void*)(vram+2*(SH-1)*SW), 0, 2*SW);
+    for (int i=SH-lines;i<SH;i++)
+    {
+        for (int j=0;j<SW;j++)
+        {
+            putcolor_internal(j, i, col);
+            putch(j, i, '\0');
+        }
+    }
 }
 
 void set_cursor(u32 x, u32 y)

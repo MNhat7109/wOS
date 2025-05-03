72c60b0c27f6b623945f7ea714e7708d44a8df6f
HarmoniousHippo09 <nvhm09@gmail.com>
2025-05-03 13:05:30 +0700

    modified:   src/bootloader/stage2/interrupt/interrupt.c 	modified:   src/bootloader/stage2/interrupt/interrupt.h 	modified:   src/bootloader/stage2/stdint.h

--- src/bootloader/stage2/interrupt/interrupt.c
+++ src/bootloader/stage2/interrupt/interrupt.c
@@ -1,21 +1,10 @@
 #include "interrupt.h"
 #include "idt.h"
 #include "isr.h"
-#include "irq.h"
-#include "../stdio.h"
-
-static int a = 0;
-void timer(registers_t* regs)
-{
-    a++;
-    kprintf("%d\n", a);
-    return;
-}
 
 void interrupt_init()
 {
     IDT_init();
     ISR_init();
     IRQ_init();
-    //IRQ_reg_handler(0, timer);
 }
--- src/bootloader/stage2/stdint.h
+++ src/bootloader/stage2/stdint.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#define NULL 0
+
 typedef unsigned char      u8;
 typedef unsigned short     u16;
 typedef unsigned long      u32;
